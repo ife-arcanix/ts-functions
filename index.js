845b58b01a8ddb3129ea0267fb68bcc6b437e010
Ife <ifebrand6@gmail.com>
2024-12-10 09:06:41 +0000

    d

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
     // APIS URLS
+    // jdsds
+    //dsds
     const deliverableUrl = "https://arcanix.ai/version-31b2p/api/1.1/obj/Deliverable/[uid]";
     const dependencyUrl = "https://arcanix.ai/version-31b2p/api/1.1/obj/Dependency/[uid]";
     const updateActivityEndpoint = "https://arcanix.ai/version-31b2p/api/1.1/wf/update_activity_time_take"
